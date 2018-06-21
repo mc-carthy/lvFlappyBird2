519b18508df2135b323a0d70fee63bda0e70a8f8
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-06-21 08:37:20 +0100

    Add visual assets

--- main.lua
+++ main.lua
@@ -1,25 +1,41 @@
-require('src.utils.debug')
+Push = require ('src.lib.push')
 
---[[
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
 
-    rm -rf .git
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288
 
-]]--
+local background = love.graphics.newImage('src/assets/sprites/background.png')
+local ground = love.graphics.newImage('src/assets/sprites/ground.png')
 
 function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
 
+    love.window.setTitle('Flappy Bird')
+
+    Push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        vsync = true,
+        fullscreen = false,
+        resizable = true
+    })
 end
 
-function love.update(dt)
-
-end
-
-function love.draw()
-
+function love.resize(w, h)
+    Push:resize(w, h)
 end
 
 function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
     end
+end
+
+function love.draw()
+    Push:start()
+
+    love.graphics.draw(background, 0, 0)
+    love.graphics.draw(ground, 0, VIRTUAL_HEIGHT - 16)
+    
+    Push:finish()
 end
